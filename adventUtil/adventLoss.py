ade704a17454edf5bf0e06c6092b95ab8ad2f694
Kim Seonggon <68697884+sungonuni@users.noreply.github.com>
2021-07-14 10:25:04 +0900

    Delete adventUtil directory

--- adventUtil/adventLoss.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn as nn
-
-def bce_loss(y_pred, y_label):
-    y_label_tensor = torch.FloatTensor(y_pred.size())
-    y_label_tensor.fill_(y_label)
-    y_label_tensor = y_label_tensor.to(y_pred.get_device())
-    return nn.BCEWithLogitsLoss()(y_pred, y_label_tensor)
